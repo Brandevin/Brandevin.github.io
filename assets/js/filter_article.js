529c702cc4676d88151d9ef8cea1c350c243e04e
Brandevin <felipe.courasuzuki@gmail.com>
2021-08-29 22:48:29 -0300

    Added proper references to article, images and repository

--- assets/js/filter_article.js
+++ assets/js/filter_article.js
@@ -4,12 +4,16 @@ function filterSelection() {
   x = document.getElementsByClassName("filterDiv");
   filter1=document.getElementsByClassName('selector')[0].value
   filter2=document.getElementsByClassName('selector')[1].value
+  filter3=document.getElementsByClassName('selector')[2].value
+
   if (filter1 == "all") filter1 = "";
   if (filter2 == "all") filter2 = "";
+  if (filter3 == "all") filter3 = "";
+
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(filter1) > -1 && x[i].className.indexOf(filter2) > -1) w3AddClass(x[i], "show");
+    if (x[i].className.indexOf(filter1) > -1 && x[i].className.indexOf(filter2) > -1 && x[i].className.indexOf(filter3) > -1) w3AddClass(x[i], "show");
   }
 }
 
